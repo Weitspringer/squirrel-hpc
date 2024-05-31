56bb397bef44427e6e3ed75c6959e4048d965670
Weitspringer <luca.springer@posteo.de>
2024-05-31 17:12:16 +0200

    ✨ Print statistics

--- research/power_correlation/datalake.py
+++ research/power_correlation/datalake.py
@@ -17,17 +17,20 @@ from research.energy_data.energycharts import fetch_public_power
 # Get slurm job data for the node
 node = "gx02"
 country = "de"
-slurm_file = Path("research") / "data" / "datalake" / "slurm.csv"
+root_folder = Path("research") / "data" / "datalake"
+slurm_file = root_folder / "slurm.csv"
 slurm_df = pd.read_csv(slurm_file)
 slurm_df = slurm_df[slurm_df["Nodelist"] == node]
 slurm_df["Start"] = pd.to_datetime(slurm_df["Start"])
 slurm_df["End"] = pd.to_datetime(slurm_df["End"])
 slurm_df["Duration"] = slurm_df["End"] - slurm_df["Start"]
 slurm_df = slurm_df.sort_values(by="Start")
-slurm_df = slurm_df[slurm_df["Duration"] >= pd.Timedelta(minutes=10)].reset_index()
+slurm_df_filtered = slurm_df[
+    slurm_df["Duration"] >= pd.Timedelta(minutes=10)
+].reset_index()
 
 # Get node power data
-node_file = Path("research") / "data" / "datalake" / "power" / f"{node}.csv"
+node_file = root_folder / "power" / f"{node}.csv"
 node_df = pd.read_csv(node_file)
 node_df["Time"] = pd.to_datetime(node_df["Time"])
 start_date = node_df["Time"].min()
@@ -37,10 +40,33 @@ public_power = fetch_public_power(country=country, start=start_date, end=end_dat
 carbon_intensities = get_carbon_intensity(country=country, response=public_power)
 
 ########
+# STATISTICS
+########
+
+# Job duration
+print("Median job duration:", slurm_df["Duration"].median().value / 3.6e12, "hours")
+print(
+    "Median job duration (≥ 10 min):",
+    slurm_df_filtered["Duration"].median().value / 3.6e12,
+    "hours",
+)
+
+# Pearson Correlation Coefficient of grid carbon intensity and node power
+node_power_interp = np.interp(
+    x=carbon_intensities["unix_seconds"],
+    xp=list(map(datetime.timestamp, node_df["Time"])),
+    fp=node_df["Power Consumption [W]"],
+)
+print(
+    "PCC of grid carbon intensity and node power:",
+    np.corrcoef(carbon_intensities["data"], node_power_interp),
+)
+
+########
 # PLOTTING
 ########
 
-_, ax = plt.subplots(layout="tight")
+_, ax = plt.subplots(figsize=(16, 10), layout="tight")
 
 ##### POWER DATA
 (p1,) = ax.plot(
@@ -56,7 +82,7 @@ ax.tick_params(axis="y", colors=p1.get_color())
 
 ##### JOBS
 twin = ax.twinx()
-for index, job in slurm_df.iterrows():
+for index, job in slurm_df_filtered.iterrows():
     current_height = index + 1
     twin.hlines(
         y=current_height,
@@ -74,28 +100,19 @@ twin.tick_params(axis="y", colors="C1")
 twin2 = ax.twinx()
 ts = list(map(datetime.fromtimestamp, carbon_intensities["unix_seconds"]))
 (p3,) = twin2.plot(
-    ts, carbon_intensities["data"], color="black", alpha=0.3, label="Carbon Intensity"
+    ts,
+    carbon_intensities["data"],
+    color="black",
+    alpha=0.3,
+    label="Grid Carbon Intensity",
 )
-twin2.set(ylabel="gCO2-eq./KWh")
+twin2.set(ylabel="g$CO_{2}$-eq./KWh")
 twin2.yaxis.label.set_color("black")
 twin2.tick_params(axis="y", colors="black")
 twin2.spines["right"].set_position(("outward", 60))
-
-# Interpolate node power consumption function
-# to match sample points from carbon intensity
-node_power_interp = np.interp(
-    x=carbon_intensities["unix_seconds"],
-    xp=list(map(datetime.timestamp, node_df["Time"])),
-    fp=node_df["Power Consumption [W]"],
-)
-# Calculate Pearson Correlation Coefficient
-print(
-    "PCC = ",
-    np.corrcoef(carbon_intensities["data"], node_power_interp),
-)
 
 ##### MISC
 plt.gcf().autofmt_xdate()
 plt.title(f"Power consumption and jobs on node {node}")
 ax.legend(handles=[p1, p3])
-plt.show()
+plt.savefig(fname=root_folder / "analysis.svg")
