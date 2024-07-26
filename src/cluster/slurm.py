e7514931206cc8261b5e6e60da93d5e247a8c4c2
Weitspringer <luca.springer@posteo.de>
2024-07-26 13:05:01 +0000

    Add some Slurm functionality for jobs based on scontrol and scancel

---
+++ src/cluster/slurm.py
@@ -0,0 +1,60 @@
+"""Manage the Slurm cluster.
+
+Many of these functions are adapted from GreenSlot (Goiri et al., 2015).
+DOI: 10.1016/j.adhoc.2014.11.012
+https://github.com/goiri/greenslot/blob/master/gslurmcommons.py
+"""
+
+from subprocess import call, PIPE
+
+
+def set_job_priority(job_id: str, priority: str) -> int:
+    """Change the priority of a job.
+
+    Args:
+        job_id (str): Job ID.
+        priority (str): Priority
+
+    Returns:
+        int: Return code of scontrol.
+    """
+    return call(
+        ["scontrol", "-o", "update", "JobId=" + job_id, "Priority=" + priority],
+        stderr=PIPE,
+    )
+
+
+def suspend_job(job_id: str) -> int:
+    """Suspend a job.
+
+    Args:
+        job_id (str): Job ID.
+
+    Returns:
+        int: Return code of scontrol.
+    """
+    return call(["scontrol", "suspend", job_id], stderr=PIPE)
+
+
+def resume_job(job_id: str) -> int:
+    """Resume a suspended job.
+
+    Args:
+        job_id (str): Job ID.
+
+    Returns:
+        int: Return code of scontrol.
+    """
+    return call(["scontrol", "resume", job_id], stderr=PIPE)
+
+
+def cancel_job(job_id: str) -> int:
+    """Cancel a job.
+
+    Args:
+        job_id (str): Job ID.
+
+    Returns:
+        int: Return code of scancel.
+    """
+    return call(["scancel", job_id], stderr=PIPE)
