873aec27f420da478f6b3bf600d152891c93134b
Weitspringer <luca.springer@posteo.de>
2024-09-09 15:05:34 +0000

    Refactor experiment to reuse code

---
+++ simulation/common/scheduling.py
@@ -0,0 +1,294 @@
+from datetime import datetime, timedelta
+from multiprocessing import Queue, Process
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from src.config.squirrel_conf import Config
+from src.data.influxdb import get_gci_data
+from src.sched.scheduler import Scheduler, PlanningStrategy
+from src.sched.timetable import Timetable
+
+
+def _sim_schedule(
+    strategy: PlanningStrategy,
+    submit_date: datetime,
+    influx_options: dict,
+    jobs: dict[str, dict[str, int]],
+    cluster_path: Path,
+    hours: int = 24,
+) -> tuple[float, float]:
+    """Schedule a job set, all with the same submit date.
+    Returns scheduling footprint and average job delay.
+    """
+    # Create scheduler
+    scheduler = Scheduler(
+        strategy=strategy,
+        cluster_info=cluster_path,
+    )
+    # Construct new time tables
+    # Append ground truth data to new timetables (no forecasting error)
+    timetable = Timetable()
+    timetable.append_historic(
+        start=submit_date,
+        end=submit_date + timedelta(hours=hours),
+        options=influx_options,
+    )
+    for job_id in jobs.keys():
+        scheduler.schedule_sbatch(
+            timetable=timetable, job_id=job_id, hours=1, partitions=["admin"]
+        )
+    footprint = 0
+    delays = []
+    for index, slot in enumerate(timetable.timeslots):
+        if len(slot.reserved_resources) == 1:
+            for key, consumptions in jobs.items():
+                reservation = slot.get_reservation(key)
+                if reservation:
+                    watts = consumptions.get(reservation.get("node"))
+                    delays.append(index)
+                    footprint += slot.gci * (
+                        (watts / 1000)
+                        * (
+                            (
+                                datetime.fromisoformat(reservation.get("end"))
+                                - datetime.fromisoformat(reservation.get("start"))
+                            ).seconds
+                            / 60
+                            / 60
+                        )
+                    )
+        elif len(slot.reserved_resources) > 1:
+            raise RuntimeError(
+                "Please use single-node configuration for this experiment!"
+            )
+    return footprint, np.average(delays)
+
+
+def _sim_schedule_forecasted(
+    strategy: PlanningStrategy,
+    start_date: str,
+    submit_date: datetime,
+    influx_options: dict,
+    jobs: dict[str, dict[str, int]],
+    cluster_path: Path,
+    hours: int = 24,
+) -> tuple[float, float]:
+    """Schedule a job set, all with the same submit date.
+    The GCI is not set from history, but forecasted.
+    Returns scheduling footprint and average job delay.
+    """
+    # Get ground truth GCI data
+    gci_hist = get_gci_data(
+        start=datetime.fromisoformat(start_date),
+        stop=datetime.fromisoformat(start_date) + timedelta(days=hours + 1),
+        options=influx_options,
+    )
+    # Create scheduler
+    scheduler = Scheduler(
+        strategy=strategy,
+        cluster_info=cluster_path,
+    )
+    # Construct new time tables
+    timetable = Timetable()
+    timetable.append_forecast(
+        start=submit_date,
+        forecast_days=1,
+        lookback_days=2,
+        options=influx_options,
+    )
+    for job_id in jobs.keys():
+        scheduler.schedule_sbatch(
+            timetable=timetable, job_id=job_id, hours=1, partitions=["admin"]
+        )
+    footprint = 0
+    delays = []
+    for index, slot in enumerate(timetable.timeslots):
+        if len(slot.reserved_resources) == 1:
+            for key, consumptions in jobs.items():
+                reservation = slot.get_reservation(key)
+                if reservation:
+                    watts = consumptions.get(reservation.get("node"))
+                    delays.append(index)
+                    real_gci = gci_hist.loc[
+                        gci_hist["time"] == pd.Timestamp(slot.start)
+                    ].iloc[0]["gci"]
+                    footprint += real_gci * (
+                        (watts / 1000)
+                        * (
+                            (
+                                datetime.fromisoformat(reservation.get("end"))
+                                - datetime.fromisoformat(reservation.get("start"))
+                            ).seconds
+                            / 60
+                            / 60
+                        )
+                    )
+        elif len(slot.reserved_resources) > 1:
+            raise RuntimeError(
+                "Please use single-node configuration for this experiment!"
+            )
+    return footprint, np.average(delays)
+
+
+def _compare(
+    zone: str,
+    start: str,
+    days: int,
+    lookahead_hours: int,
+    jobs: dict[str, dict[str, int]],
+    cluster_path: Path,
+    strat_1: PlanningStrategy,
+    strat_2: PlanningStrategy,
+    forecasted: bool,
+    queue: Queue,
+) -> None:
+    # Create date range for job submissions
+    first_submit_date = datetime.fromisoformat(start)
+    submit_dates = pd.date_range(
+        start=first_submit_date,
+        periods=days * lookahead_hours,
+        freq="h",
+        tz="UTC",
+    )
+    footprints_1 = []
+    delays_1 = []
+    footprints_2 = []
+    delays_2 = []
+    influx_options = Config.get_influx_config()["gci"]["history"]
+    influx_options.get("tags").update({"zone": zone})
+    for submit_date in submit_dates:
+        if not forecasted:
+            footprint_1, delay_1 = _sim_schedule(
+                strategy=strat_1,
+                submit_date=submit_date,
+                influx_options=influx_options,
+                jobs=jobs,
+                cluster_path=cluster_path,
+                hours=lookahead_hours,
+            )
+            footprint_2, delay_2 = _sim_schedule(
+                strategy=strat_2,
+                submit_date=submit_date,
+                influx_options=influx_options,
+                jobs=jobs,
+                cluster_path=cluster_path,
+                hours=lookahead_hours,
+            )
+        else:
+            footprint_1, delay_1 = _sim_schedule_forecasted(
+                strategy=strat_1,
+                submit_date=submit_date,
+                start_date=start,
+                influx_options=influx_options,
+                jobs=jobs,
+                cluster_path=cluster_path,
+                hours=lookahead_hours,
+            )
+            footprint_2, delay_2 = _sim_schedule_forecasted(
+                strategy=strat_2,
+                submit_date=submit_date,
+                start_date=start,
+                influx_options=influx_options,
+                jobs=jobs,
+                cluster_path=cluster_path,
+                hours=lookahead_hours,
+            )
+        footprints_1.append(round(footprint_1, 2))
+        delays_1.append(delay_1)
+        footprints_2.append(round(footprint_2, 2))
+        delays_2.append(delay_2)
+    queue.put(
+        (
+            zone,
+            {
+                "fp_1": footprints_1,
+                "d_1": delays_1,
+                "fp_2": footprints_2,
+                "d_2": delays_2,
+            },
+        )
+    )
+
+
+def main(
+    zones: list[str],
+    start: str,
+    days: int,
+    lookahead_hours: int,
+    jobs: dict[str, dict[str, int]],
+    cluster_path: Path,
+    result_dir: Path,
+    strat_1: PlanningStrategy,
+    strat_2: PlanningStrategy,
+    forecasting: bool,
+) -> None:
+    result_dir.mkdir(exist_ok=True, parents=True)
+    # Scheduling for multiple timetables
+    zoned_1_footprints = {}
+    zoned_1_delays = {}
+    zoned_2_footprints = {}
+    zoned_2_delays = {}
+    q = Queue()
+    for z in zones:
+        p = Process(
+            target=_compare,
+            args=(
+                z,
+                start,
+                days,
+                lookahead_hours,
+                jobs,
+                cluster_path,
+                strat_1,
+                strat_2,
+                forecasting,
+                q,
+            ),
+        )
+        p.start()
+    for _ in range(len(zones)):
+        z, results = q.get()
+        zoned_1_footprints.update({z: results.get("fp_1")})
+        zoned_1_delays.update({z: results.get("d_1")})
+        zoned_2_footprints.update({z: results.get("fp_2")})
+        zoned_2_delays.update({z: results.get("d_2")})
+    zoned_1_footprints = dict(sorted(zoned_1_footprints.items()))
+    zoned_1_delays = dict(sorted(zoned_1_delays.items()))
+    zoned_2_footprints = dict(sorted(zoned_2_footprints.items()))
+    zoned_2_delays = dict(sorted(zoned_2_delays.items()))
+
+    ##### Plotting
+    hours = range(24)
+
+    # Plot savings per hour of day
+    for zone, ts_footprints in zoned_2_footprints.items():
+        ts_savings_hourly_median = np.ma.median(
+            np.subtract(
+                1, np.divide(ts_footprints, zoned_1_footprints.get(zone))
+            ).reshape((days, 24)),
+            axis=0,
+        )
+        plt.plot(hours, ts_savings_hourly_median, label=zone)
+    plt.ylabel("Median Fraction of Emissions Saved")
+    plt.xlabel("Hour of Day")
+    plt.legend(loc="upper left", ncols=len(zones))
+    plt.ylim(-0.2, 1)
+    plt.grid(axis="y", linewidth=0.5)
+    plt.tight_layout()
+    plt.savefig(result_dir / "result.png")
+    plt.clf()
+
+    ## Plot average job delay
+    for zone, ts_delays in zoned_2_delays.items():
+        ts_hourly_delay = np.ma.median(np.reshape(ts_delays, (days, 24)), axis=0)
+        # ca_hourly_delay = np.ma.median(np.reshape(ca_delays, (DAYS, 24)), axis=0)
+        # plt.plot(hours, ca_hourly_delay, color="blue", label="FIFO")
+        plt.plot(hours, ts_hourly_delay, label=zone)
+    plt.ylabel("Avg. Delay [hours]")
+    plt.xlabel("Hour of Day")
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(result_dir / "delay.png")
--- simulation/exp1/forecast.py
+++ simulation/exp1/forecast.py
@@ -1,203 +1,36 @@
 """Timeshifting experiment."""
 
-from datetime import datetime, timedelta
-from multiprocessing import Queue, Process
 from pathlib import Path
 
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-
 from src.config.squirrel_conf import Config
-from src.data.influxdb import get_gci_data
-from src.sched.scheduler import Scheduler, TemporalShifting, CarbonAgnosticFifo
-from src.sched.timetable import Timetable
+from src.sched.scheduler import CarbonAgnosticFifo, TemporalShifting
+
+from simulation.common.scheduling import main
 
 # Experiment configuration
 ZONES = ["DE", "FR", "GB", "PL", "US-MIDA-PJM"]
-START = "2023-01-03T00:00:00+00:00"
-DAYS = 362
-JOBS = {"job1": 250, "job2": 200, "job3": 150, "job4": 100, "job5": 50}
+START = "2023-08-01T00:00:00+00:00"
+DAYS = 10
+JOBS = {
+    "job1": {"c1": 250},
+    "job2": {"c1": 200},
+    "job3": {"c1": 150},
+    "job4": {"c1": 100},
+    "job5": {"c1": 50},
+}
+LOOKAHEAD_HOURS = 24
 CLUSTER_PATH = Path("simulation") / "data" / "single-node-cluster.json"
 RESULT_DIR = Config.get_local_paths()["viz_path"] / "simulation" / "exp1" / "forecast"
-RESULT_DIR.mkdir(exist_ok=True, parents=True)
 
-
-def _sim_schedule(
-    submit_date: datetime, influx_options: dict
-) -> tuple[float, float, float, float]:
-    # Get ground truth GCI data
-    gci_hist = get_gci_data(
-        start=datetime.fromisoformat(START),
-        stop=datetime.fromisoformat(START) + timedelta(days=DAYS + 1),
-        options=influx_options,
-    )
-    # Create schedulers
-    carbon_agnostic_scheduler = Scheduler(
-        strategy=CarbonAgnosticFifo(),
-        cluster_info=CLUSTER_PATH,
-    )
-    timeshift_scheduler = Scheduler(
-        strategy=TemporalShifting(),
-        cluster_info=CLUSTER_PATH,
-    )
-    # Construct new time tables
-    # Append ground truth data to new timetables (no forecasting error)
-    ca_timetable = Timetable()
-    ca_timetable.append_forecast(
-        start=submit_date,
-        forecast_days=1,
-        lookback_days=2,
-        options=influx_options,
-    )
-    ts_timetable = Timetable()
-    ts_timetable.append_forecast(
-        start=submit_date,
-        forecast_days=1,
-        lookback_days=2,
-        options=influx_options,
-    )
-    for job_id in JOBS.keys():
-        carbon_agnostic_scheduler.schedule_sbatch(
-            timetable=ca_timetable, job_id=job_id, hours=1, partitions=["admin"]
-        )
-        timeshift_scheduler.schedule_sbatch(
-            timetable=ts_timetable, job_id=job_id, hours=1, partitions=["admin"]
-        )
-    # Calculate carbon-agnostic emissions and average delay
-    ca_footprint = 0
-    ca_delays = []
-    for index, slot in enumerate(ca_timetable.timeslots):
-        if len(slot.reserved_resources) == 1:
-            for key, watts in JOBS.items():
-                reservation = slot.get_reservation(key)
-                if reservation:
-                    ca_delays.append(index)
-                    real_gci = gci_hist.loc[
-                        gci_hist["time"] == pd.Timestamp(slot.start)
-                    ].iloc[0]["gci"]
-                    ca_footprint += real_gci * (watts / 1000)
-        elif len(slot.reserved_resources) > 1:
-            raise RuntimeError(
-                "Please use single-node configuration for this experiment!"
-            )
-    # Calculate timeshifting emissions and average delay
-    ts_footprint = 0
-    ts_delays = []
-    for index, slot in enumerate(ts_timetable.timeslots):
-        if len(slot.reserved_resources) == 1:
-            for key, watts in JOBS.items():
-                reservation = slot.get_reservation(key)
-                if reservation:
-                    ts_delays.append(index)
-                    real_gci = gci_hist.loc[
-                        gci_hist["time"] == pd.Timestamp(slot.start)
-                    ].iloc[0]["gci"]
-                    ts_footprint += real_gci * (
-                        (watts / 1000)
-                        * (
-                            (
-                                datetime.fromisoformat(reservation.get("end"))
-                                - datetime.fromisoformat(reservation.get("start"))
-                            ).seconds
-                            / 60
-                            / 60
-                        )
-                    )
-        elif len(slot.reserved_resources) > 1:
-            raise RuntimeError(
-                "Please use single-node configuration for this experiment!"
-            )
-    return ca_footprint, np.average(ca_delays), ts_footprint, np.average(ts_delays)
-
-
-def _simulate(zone: str, queue: Queue) -> tuple[list, list, list, list, list]:
-    # Create date range for job submissions
-    first_submit_date = datetime.fromisoformat(START)
-    submit_dates = pd.date_range(
-        start=first_submit_date,
-        periods=DAYS * 24,
-        freq="h",
-        tz="UTC",
-    )
-    ca_footprints = []
-    ca_delays = []
-    ts_footprints = []
-    ts_delays = []
-    influx_options = Config.get_influx_config()["gci"]["history"]
-    influx_options.get("tags").update({"zone": zone})
-    for submit_date in submit_dates:
-        ca_footprint, ca_delay, ts_footprint, ts_delay = _sim_schedule(
-            submit_date=submit_date, influx_options=influx_options
-        )
-        ca_footprints.append(round(ca_footprint, 2))
-        ca_delays.append(ca_delay)
-        ts_footprints.append(round(ts_footprint, 2))
-        ts_delays.append(ts_delay)
-    queue.put(
-        (
-            zone,
-            {
-                "ca_fp": ca_footprints,
-                "ca_d": ca_delays,
-                "ts_fp": ts_footprints,
-                "ts_d": ts_delays,
-            },
-        )
-    )
-
-
-# Scheduling for multiple timetables
-zoned_ca_footprints = {}
-zoned_ca_delays = {}
-zoned_ts_footprints = {}
-zoned_ts_delays = {}
-q = Queue()
-for z in ZONES:
-    p = Process(target=_simulate, args=(z, q))
-    p.start()
-for _ in range(len(ZONES)):
-    z, results = q.get()
-    zoned_ca_footprints.update({z: results.get("ca_fp")})
-    zoned_ca_delays.update({z: results.get("ca_d")})
-    zoned_ts_footprints.update({z: results.get("ts_fp")})
-    zoned_ts_delays.update({z: results.get("ts_d")})
-zoned_ca_footprints = dict(sorted(zoned_ca_footprints.items()))
-zoned_ca_delays = dict(sorted(zoned_ca_delays.items()))
-zoned_ts_footprints = dict(sorted(zoned_ts_footprints.items()))
-zoned_ts_delays = dict(sorted(zoned_ts_delays.items()))
-
-##### Plotting
-hours = range(24)
-
-# Plot savings per hour of day
-zoned_ts_savings_hourly_median = {}
-for zone, ts_footprints in zoned_ts_footprints.items():
-    ts_savings_hourly_median = np.ma.median(
-        np.subtract(1, np.divide(ts_footprints, zoned_ca_footprints.get(zone))).reshape(
-            (DAYS, 24)
-        ),
-        axis=0,
-    )
-    plt.plot(hours, ts_savings_hourly_median, label=zone)
-plt.ylabel("Median Fraction of Emissions Saved")
-# plt.title("E1: Median Fractional Carbon Savings of Timeshifting (vs. FIFO)")
-plt.xlabel("Hour of Day")
-plt.legend(loc="upper left", ncols=len(ZONES))
-plt.ylim(-0.2, 1)
-plt.grid(axis="y", linewidth=0.5)
-plt.tight_layout()
-plt.savefig(RESULT_DIR / "result.png")
-plt.clf()
-
-## Plot average job delay
-for zone, ts_delays in zoned_ts_delays.items():
-    ts_hourly_delay = np.ma.median(np.reshape(ts_delays, (DAYS, 24)), axis=0)
-    # ca_hourly_delay = np.ma.median(np.reshape(ca_delays, (DAYS, 24)), axis=0)
-    # plt.plot(hours, ca_hourly_delay, color="blue", label="FIFO")
-    plt.plot(hours, ts_hourly_delay, label=zone)
-plt.ylabel("Avg. Delay [hours]")
-plt.xlabel("Hour of Day")
-plt.legend()
-plt.tight_layout()
-plt.savefig(RESULT_DIR / "delay.png")
+main(
+    zones=ZONES,
+    start=START,
+    days=DAYS,
+    lookahead_hours=LOOKAHEAD_HOURS,
+    jobs=JOBS,
+    cluster_path=CLUSTER_PATH,
+    result_dir=RESULT_DIR,
+    strat_1=CarbonAgnosticFifo(),
+    strat_2=TemporalShifting(),
+    forecasting=True,
+)
