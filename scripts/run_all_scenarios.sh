809c0489cf283635e1a982adb8f65930d7f0cd56
Weitspringer <luca.springer@posteo.de>
2024-09-12 14:38:47 +0000

    Add script to run all scenarios

---
+++ scripts/run_all_scenarios.sh
@@ -0,0 +1,6 @@
+python -m cli scenarios timeshift asc
+python -m cli scenarios timeshift desc
+python -m cli scenarios timeshift cons
+python -m cli scenarios timeshift desc+forecast
+python -m src.sim.exp2.greedy
+python -m src.sim.exp2.loadbalancer
