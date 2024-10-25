5d75758d8bfe15623f06145abf4f90b09b5e1cc4
Weitspringer <luca.springer@posteo.de>
2024-10-25 11:17:44 +0200

    Adjust evaluation script

--- scripts/evaluations/run_scenario1.sh
+++ scripts/evaluations/run_scenario1.sh
@@ -2,6 +2,5 @@ python -m cli scenarios temporal asc
 python -m cli scenarios temporal desc
 python -m cli scenarios temporal cons
 python -m cli scenarios temporal desc+forecast
-python -m cli scenarios temporal cons+multi
 python -m cli scenarios temporal chronus
 python -m cli scenarios visualize
