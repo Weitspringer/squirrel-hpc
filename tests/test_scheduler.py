a463c1593847340a7ebfaf583793851272a1d864
Weitspringer <luca.springer@posteo.de>
2024-10-26 11:53:05 +0200

    ✅ Update tests

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -15,12 +15,12 @@ class TestSlurmCommons(unittest.TestCase):
         # Instantiate the class and call _get_nodes
         obj = mut.Scheduler(
             strategy=mut.CarbonAgnosticFifo(),
-            cluster_info=Path("src") / "sim" / "data" / "multi-node-cluster.json",
+            cluster_info=Path("src") / "sim" / "data" / "3-node-cluster.json",
         )
-        result = obj._get_nodes(partitions=["simsala"], num_gpus=1)
+        result = obj._get_nodes(partitions=["sorcery"], num_gpus=1)
 
         # Expected result after sorting by weight and name
-        expected_result = ["g01"]
+        expected_result = ["gx03"]
 
         # Check if the result matches the expected nodes
         self.assertEqual(result, expected_result)
@@ -31,9 +31,9 @@ class TestSlurmCommons(unittest.TestCase):
         # Instantiate the class and call _get_nodes
         obj = mut.Scheduler(
             strategy=mut.CarbonAgnosticFifo(),
-            cluster_info=Path("src") / "sim" / "data" / "multi-node-cluster.json",
+            cluster_info=Path("src") / "sim" / "data" / "3-node-cluster.json",
         )
-        result = obj._get_nodes(partitions=["simsala"], num_gpus=3)
+        result = obj._get_nodes(partitions=["sorcery"], num_gpus=3)
 
         # Expected result after sorting by weight and name
         expected_result = []
@@ -47,12 +47,12 @@ class TestSlurmCommons(unittest.TestCase):
         # Instantiate the class and call _get_nodes
         obj = mut.Scheduler(
             strategy=mut.CarbonAgnosticFifo(),
-            cluster_info=Path("src") / "sim" / "data" / "multi-node-cluster.json",
+            cluster_info=Path("src") / "sim" / "data" / "3-node-cluster.json",
         )
-        result = obj._get_nodes(partitions=["kadabra"])
+        result = obj._get_nodes(partitions=["jinx"])
 
         # Expected result after sorting by weight and name
-        expected_result = ["c01", "c02", "c03", "g01"]
+        expected_result = ["cx16", "cx17", "gx03"]
 
         # Check if the result matches the expected nodes
         self.assertEqual(result, expected_result)
